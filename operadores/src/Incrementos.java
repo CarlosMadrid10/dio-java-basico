6eda4c837ad092736dc9e85e9c61af9094b99ada
CarlosMadrid10 <carloseduardomadrid.2@gmail.com>
2025-02-22 16:36:58 -0300

    Operadores Ternarios

---
+++ operadores/src/Incrementos.java
@@ -0,0 +1,25 @@
+public class Incrementos {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //numero = numero + 1;
+
+        //Apresenta o número e depois incrementa
+        System.out.println(numero++);
+        System.out.println(numero);
+
+        //Incrementa o valor e depois apresenta
+        System.out.println(++numero);
+        System.out.println(numero);
+
+        // ! | É o sinal da negação
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+
+}
