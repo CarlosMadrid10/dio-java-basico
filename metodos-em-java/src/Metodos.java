bca0536cbffe41f15ce3413ab87307608d9b0020
CarlosMadrid10 <carloseduardomadrid.2@gmail.com>
2025-02-22 23:28:52 -0300

    sistema-smart-tv

--- metodos-em-java/src/Metodos.java
+++ metodos-em-java/src/Metodos.java
@@ -1,5 +1,18 @@
 public class Metodos {
     public static void main(String[] args) throws Exception {
+
+        //Conceito
+        //Classe: composta por atributos e métodos
+        //Atributo: Atributos são variáveis de diferentes tipos e valores.
+        //Método: podem ser ações, funções ou sub-rotinas disponíveis dentro da classe.
+
+        //Convenções
+        //Métodos devem ser escritos como verbo e seguir o padrão camelCase.
+        //Métodos devem descrever a finalidade do seu bloco de código.
+        //Métodos devem possuir apenas uma responsabilidade.
+        //Métodos em Java não podem ser globais.
+        //É de suma importância verificar os retornos e a visibilidade dos métodos. 
+
         System.out.println("Hello, World!");
     }
 }
