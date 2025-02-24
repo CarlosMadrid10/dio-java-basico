d7e314b1ed8d73aa6bca8d555e57799a757ee7f4
CarlosMadrid10 <carloseduardomadrid.2@gmail.com>
2025-02-24 14:29:37 -0300

    Add comment

---
+++ escopos/src/Escopo.java
@@ -0,0 +1,9 @@
+//Escopo: Pode ser entendido como o ambiente onde uma variável pode ser declarada
+//O escopo de variável vai de acordo com o bloco onde ela foi declarada
+
+
+public class Escopo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
